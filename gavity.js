cbcb4aa234102e9aab7c339d36e28294dec6d199
krushnap <smitakpatil26@gmail.com>
2019-10-16 17:31:09 +0530

    more bubbles

--- gavity.js
+++ gavity.js
@@ -128,8 +128,8 @@ class Bubble
   {
         this.canvas = null;
         this.ctx=null;
-        this.width = 500;
-        this.height = 500;
+        // this.width = window.innerWidth;
+        // this.height = window.innerHeight;
         this.scale = 1;
         this.offset = {top: 0, left: 0};
         this.bubbles=[];
@@ -137,6 +137,8 @@ class Bubble
   init()
   {
     this.canvas = document.getElementById('game-area');
+    this.canvas.height=window.innerHeight;
+    this.canvas.width=window.innerWidth;
     this.ctx = this.canvas.getContext('2d');
 
     this.offset.top = this.canvas.offsetTop;
@@ -147,8 +149,14 @@ class Bubble
   }
   render(){
 
-    let Inp=new Input(bub);
-    Inp.circle(100,100,30)
+    for(let i=0;i<10;i++)
+    {
+      let r=Math.floor(Math.random()*30);
+      let x=Math.floor(Math.random()*400);
+      let y=this.canvas.height;
+      let Inp=new Input(bub);
+      Inp.circle(x,y,r)
+    }
     // ctx.arc(this.x,this.y,this.r,0,2*Math.PI);
     // POP.Draw.rect(0, 0, POP.WIDTH, POP.HEIGHT, '#036');
 
@@ -211,10 +219,6 @@ class Input{
         bub.ctx.fill();
     }
 }
-
-
-
-
 
 window.onload=function(){
   bub=new Bubble();
